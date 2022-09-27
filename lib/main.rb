623ab677aeb941e50ce7bb8b0c785332b6ecb42f
lohikaarme <stewart.watt@gmail.com>
2022-09-26 22:31:06 -0700

    main initial setup and dependancies

--- lib/main.rb
+++ lib/main.rb
@@ -1,1 +1,17 @@
+# frozen_string_literal: true
 
+# mastermind exercise to replicate the classic game 
+
+require 'pry-byebug'
+require_relative 'game.rb'
+require_relative 'board.rb'
+require_relative 'player.rb'
+require_relative 'render.rb'
+
+# binding.pry
+
+def game_start
+  game = Game.new
+  game.play
+  # logic to play game again
+end
