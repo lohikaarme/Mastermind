9c9ac3655f19664b8db7e8002ad40a4fb06a73a4
lohikaarme <stewart.watt@gmail.com>
2023-01-08 11:12:40 -0800

    Some clean up of nameing and method nesting

--- lib/ai.rb
+++ lib/ai.rb
@@ -9,7 +9,6 @@ class AI
   @guess = [1, 1, 2, 2]
   @last_guess = []
   @key = @combo.sample
-  @hash_map = {}
 
   def self.ai_key(pegs, sym)
     key = []
@@ -32,7 +31,7 @@ class AI
     [positions]
   end
 
-  def self.hash_to_match(arg)
+  def self.array_to_hash(arg)
     Hash[arg.map { |el| [el, 0] }]
   end
 
@@ -45,24 +44,23 @@ class AI
   end
 
   def self.list_match(guess_idx, list, sign, matches)
+    hash_map = array_to_hash(list)
     list.each_with_index do |el, _idx|
       el_idx = idx_to_array(el)
-      @hash_map[el] += sign if (el_idx & guess_idx).count >= matches
+      hash_map[el] += sign if (el_idx & guess_idx).count >= matches
     end
-    @hash_map[@guess] = 0
+    hash_map[@guess] = 0
     if sign == -1
-      @hash_map.select { |_k, v| v >= 0 }
+      hash_map.select { |_k, v| v >= 0 }
     else
-      @hash_map.select { |_k, v| v.positive? }
+      hash_map.select { |_k, v| v.positive? }
     end
   end
 
   def self.matching(key, guess, list)
-    @hash_map = hash_to_match(list)
     new_list = {}
     guess_idx = idx_to_array(guess)
     case checker(key, guess)
-
     when [0]
       new_list = list_match(guess_idx, list, -1, 1)
     when [1]
@@ -73,7 +71,7 @@ class AI
       new_list = list_match(guess_idx, list, 1, 3)
     when [4]
       p 'AI wins'
-      p "key:#{@key}, Guess#{@guess}"
+      p "Key:#{@key}, Guess#{@guess}"
     end
     p new_list
     p new_list.count
@@ -85,27 +83,26 @@ class AI
   end
 
   def self.next_guess(list)
-    @hash_map = hash_to_match(list)
+    hash_map = array_to_hash(list)
     @last_guess = @guess
     list.each_with_index do |el, _idx|
       list.each_with_index do |elp, _idxp|
         elp.each_with_index do |elpp, idxpp|
-          @hash_map[el] += 1 if el[idxpp] == elpp
+          hash_map[el] += 1 if el[idxpp] == elpp
         end
       end
     end
-    @guess = @hash_map.min_by { |_k, v| v }[0]
+    @guess = hash_map.min_by { |_k, v| v }[0]
+    last_guess_check(@last_guess, @guess, list)
   end
 
   def self.last_guess_check(last_guess, guess, list)
     @guess = list.sample if guess == last_guess
   end
 
-
   def self.ai_turn
     matching(@key, @guess, @combo)
     next_guess(@combo)
-    last_guess_check(@last_guess, @guess, @combo)
   end
 
   p 5 + 5
